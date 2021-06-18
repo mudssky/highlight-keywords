44d0345366060338d8d9d794cf4c3911fda72ccc
mudssky <mudssky@gmail.com>
2021-06-18 18:01:15 +0800

    ✨ feat: test replace

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('hello webpack')
+const keywordsList = [
+  {
+    keyword: '使用',
+    color: 'yellow',
+    //     styleText: 'background:yellow;',
+  },
+]
+const oldHtml = document.body.innerHTML
+// console.log(page)
+// console.log(typeof oldHtml)
+let newHtml = oldHtml
+for (const i in keywordsList) {
+  newHtml = newHtml.replaceAll(keywordsList[i].keyword, 'hello')
+}
+document.body.innerHTML = newHtml
