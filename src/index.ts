9a3e7f58be733280a05f081743b33a446a7b5bce
mudssky <mudssky@gmail.com>
2021-06-22 00:21:13 +0800

    🐞 fix:
    fix bug  cant highlight more than one keywords ,use window.onload

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,53 @@
-interface Rule {
-  keyword: string
-  styleText: string
-  matchUrl: string
-}
-// interface RuleList
-const RuleList: Array<Rule> = [
-  {
-    keyword: '成年コミック',
-    // color: 'yellow',
-    styleText: 'background:yellow;',
-    matchUrl: 'sukebei.nyaa.si',
-  },
-  {
-    keyword: '使用',
-    styleText: 'background:red',
-    matchUrl: 'www.jianshu.com',
-  },
-]
-
-// 筛选匹配当前页面的规则
-const matchedRuleList = RuleList.filter(function (rule) {
-  // 检查是否存在matchUrl选项,如果没有直接报错,默认当作匹配所有网站处理,直接通过过滤
-  if (!rule.matchUrl) {
-    console.error('this rule should have a match url')
-    return rule
+// 因为是用的@require 方法引用脚本，就和页面引入js一样是会阻塞线程的导致页面加载不出，这边onload等页面加载完再执行
+window.onload = function () {
+  interface Rule {
+    keyword: string
+    styleText: string
+    matchUrl: string
   }
-  // 存在matchUrl选项,则过滤匹配当前页面的rule
-  if (rule.matchUrl) {
-    const urlPattern = new RegExp(rule.matchUrl)
-    if (urlPattern.test(window.location.href)) {
+
+  // const defaultStyleText = 'background:gold;'
+  // interface RuleList
+  const RuleList: Array<Rule> = [
+    {
+      keyword: '成年コミック',
+      // color: 'yellow',
+      styleText: 'background:gold;',
+      matchUrl: 'sukebei.nyaa.si',
+    },
+  ]
+
+  // 筛选匹配当前页面的规则
+  const matchedRuleList = RuleList.filter(function (rule) {
+    // 检查是否存在matchUrl选项,如果没有直接报错,默认当作匹配所有网站处理,直接通过过滤
+    if (!rule.matchUrl) {
+      console.error('this rule should have a match url')
       return rule
     }
+    // 存在matchUrl选项,则过滤匹配当前页面的rule
+    if (rule.matchUrl) {
+      const urlPattern = new RegExp(rule.matchUrl)
+      if (urlPattern.test(window.location.href)) {
+        return rule
+      }
+    }
+  })
+
+  // 这个变量存放最后修改后的html
+  let lastHtml = document.body.innerHTML
+  // console.log(matchedRuleList)
+  // 使用innerhtml 替换的方式,替换关键词为带高亮style的html
+  for (const i in matchedRuleList) {
+    // 作为中间变量，每次循环从上次的结果基础上进行。
+    const currentHtml = lastHtml
+    const htmlPattern = new RegExp(
+      `(<[^>]+>[^<>]*?)${matchedRuleList[i].keyword}([^<>]*?<[^>]+>)`,
+      'g'
+    )
+    lastHtml = currentHtml.replaceAll(
+      htmlPattern,
+      `$1<em style="${matchedRuleList[i].styleText}">${matchedRuleList[i].keyword}</em>$2`
+    )
   }
-})
-// console.log(matchedRuleList)
-// 使用innerhtml 替换的方式,替换关键词为带高亮style的html
-const oldHtml = document.body.innerHTML
-let newHtml = oldHtml
-for (const i in matchedRuleList) {
-  const htmlPattern = new RegExp(
-    `(<[^>]+>[^<>]*?)${matchedRuleList[i].keyword}([^<>]*?<[^>]+>)`,
-    'g'
-  )
-  newHtml = newHtml.replaceAll(
-    htmlPattern,
-    `$1<em style="${matchedRuleList[i].styleText}">${matchedRuleList[i].keyword}</em>$2`
-  )
+  document.body.innerHTML = lastHtml
 }
-document.body.innerHTML = newHtml
