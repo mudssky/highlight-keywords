82bdae92586a0576846058f74babe077acb3e789
mudssky <mudssky@gmail.com>
2023-03-05 17:23:32 +0800

    ✨ feat: update codes

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App'
+import { GM_registerMenuCommand } from '$'
+const app = createApp(App)
+const appContainer = (() => {
+  const app = document.createElement('div')
+  document.body.append(app)
+  return app
+})()
 
-createApp(App).mount(
-  (() => {
-    const app = document.createElement('div')
-    document.body.append(app)
-    return app
-  })(),
-)
+function openPanel() {
+  app.mount(appContainer)
+}
+GM_registerMenuCommand('打开配置面板', openPanel)
