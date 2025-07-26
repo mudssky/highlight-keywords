562f38f55c11067dbdccffb4821f9054c0216f87
mudssky <mudssky@gmail.com>
2025-07-27 02:38:05 +0800

    feat: 添加tailwindcss/vite依赖并配置vite插件

--- vite.config.ts
+++ vite.config.ts
@@ -9,10 +9,12 @@ import vue from '@vitejs/plugin-vue'
 
 import monkey, { cdn, util } from 'vite-plugin-monkey'
 import packageJson from './package.json'
+import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig(async ({ mode }) => ({
   plugins: [
     vue(),
+    tailwindcss(),
     // AutoImport({
     //   resolvers: [ElementPlusResolver()],
     // }),
